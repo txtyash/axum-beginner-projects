4653353af87c92539f86f4ffd747eb96f58c1904
evccyr <evccyr@gmail.com>
2024-01-09 00:10:07 +0530

    Added Index Route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let routers = Router::new().route("/", get(root));
+
+    Ok(routers.into())
+}
+
+async fn root() -> Html<&'static str> {
+    include_str!("index.html").into()
 }
