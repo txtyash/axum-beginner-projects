156b1fe90a3554ac7193a60694b61289c90bbc4b
evccyr <evccyr@gmail.com>
2024-01-11 23:29:17 +0530

    Project Restructure

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
 
-#[tokio::main]
-async fn main() {
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
     let service =
-        ServeDir::new("assets/root").not_found_service(ServeFile::new("assets/not_found.html"));
+        ServeDir::new("assets").not_found_service(ServeFile::new("assets/not_found.html"));
     let routers = Router::new().nest_service("/", service);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, routers).await.unwrap();
+    Ok(routers.into())
 }
